913363d76418a6ff0fa68dc0d425e5d12ce2465f
Ashwin S Nambiar <76719333+Ashwin-S-Nambiar@users.noreply.github.com>
2024-10-07 10:47:44 +0530

    initial commits

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="font-mono text-3xl text-blue-900 underline">
+        Hello world!
+      </h1>
+      <p className="font-mono underline bg-green-400">TailwindCSS testing!!</p>
+    </>
+  )
+}

cc99078b7131ad7811e120f9128aa9ca6e4e1b49
Ashwin S Nambiar <76719333+Ashwin-S-Nambiar@users.noreply.github.com>
2024-10-07 15:03:50 +0530

    added some plugins.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 export default function App() {
   return (
     <>
-      <h1 className="font-mono text-3xl text-blue-900 underline">
+      <h1 className="mb-2 text-3xl font-bold text-center text-blue-900 border-4 border-cyan-700">
         Hello world!
       </h1>
-      <p className="font-mono underline bg-green-400">TailwindCSS testing!!</p>
+      <p className="mt-4 font-mono text-2xl text-center underline">TailwindCSS testing!!</p>
     </>
   )
 }

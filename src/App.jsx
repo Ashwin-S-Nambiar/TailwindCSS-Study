7117006af3a891e75258de253a2515e9ffce8fc7
Ashwin S Nambiar <76719333+Ashwin-S-Nambiar@users.noreply.github.com>
2024-10-09 11:02:39 +0530

    learning margin, padding properties.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ export default function App() {
       <h1 className="mb-2 text-3xl font-bold text-center text-blue-900 border-4 border-cyan-700">
         Hello world!
       </h1>
-      <p className="mt-4 font-mono text-2xl text-center underline">TailwindCSS testing!!</p>
+      <p className="m-4 font-mono text-2xl italic text-center underline capitalize decoration-wavy">TailwindCSS testing!!</p>
+      <div className="px-1 py-2 text-xl font-bold text-white bg-blue-800">It's a Div</div>
     </>
   )
 }

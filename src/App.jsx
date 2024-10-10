c071523b5a96c1e6ad1056cdf37a75885f8361c8
Ashwin S Nambiar <76719333+Ashwin-S-Nambiar@users.noreply.github.com>
2024-10-10 11:23:09 +0530

    height and width properties as well as flex

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 export default function App() {
   return (
-    <>
-      <h1 className="mb-2 text-3xl font-bold text-center text-blue-900 border-4 border-cyan-700">
-        Hello world!
-      </h1>
-      <p className="m-4 font-mono text-2xl italic text-center underline capitalize decoration-wavy">TailwindCSS testing!!</p>
-      <div className="px-1 py-2 text-xl font-bold text-white bg-blue-800">It's a Div</div>
-    </>
+    <main>
+      <div className="flex justify-between">
+        <div className="w-16 h-16 bg-blue-500 rounded-full"></div>
+        <div className="w-16 h-16 bg-blue-500 rounded-full"></div>
+        <div className="w-16 h-16 bg-blue-500 rounded-full"></div>
+      </div>
+    </main>
   )
 }

abfa2d148d441c11cbd244cbb2dcb91598491be2
Ashwin S Nambiar <76719333+Ashwin-S-Nambiar@users.noreply.github.com>
2024-10-11 14:38:10 +0530

    made a basic structure for the practice site.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+import {     
+  Hero,
+  Footer,
+  CustomerReviews,
+  Services,
+  Subscribe,
+  SpecialOffer,
+  SuperQuality,
+  PopularProducts
+} from './sections/index'
+
+const App = () => (
+  <main className="relative">
+      {/* <Nav /> */}
+    <section className="x1:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section> 
+    <section className="padding">
+      <SuperQuality />
+    </section> 
+    <section className="py-10 padding-x<">
+      <Services />
+    </section> 
+    <section className="padding">
+      <SpecialOffer />
+    </section> 
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section> 
+    <section className="w-full py-16 padding-x sm:py-32">
+      <Subscribe />
+    </section> 
+    <section className="pb-8 bg-black padding-x padding-t">
+      <Footer />
+    </section> 
+  </main> 
+)
+
+export default App
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import Footer from './Footer'
+import CustomerReviews from './CustomerReviews'
+import Services from './Services'
+import Subscribe from './Subscribe'
+import SpecialOffer from './SpecialOffer'
+import SuperQuality from './SuperQuality'
+import PopularProducts from './PopularProducts'
+
+export {
+    Hero,
+    Footer,
+    CustomerReviews,
+    Services,
+    Subscribe,
+    SpecialOffer,
+    SuperQuality,
+    PopularProducts
+}

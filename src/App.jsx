1da9c58651d8d18f311a854391126e01604eab9b
Ashwin S Nambiar <76719333+Ashwin-S-Nambiar@users.noreply.github.com>
2024-10-11 11:41:31 +0530

    added assets.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 export default function App() {
   return (
-    <main>
-      <div className="flex justify-between">
-        <div className="w-16 h-16 bg-blue-500 rounded-full"></div>
-        <div className="w-16 h-16 bg-blue-500 rounded-full"></div>
-        <div className="w-16 h-16 bg-blue-500 rounded-full"></div>
-      </div>
-    </main>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }

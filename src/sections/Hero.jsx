11231f38ef060324b517f8f940ee5a4eafaea99c
Ashwin S Nambiar <76719333+Ashwin-S-Nambiar@users.noreply.github.com>
2024-10-13 07:44:04 +0530

    added some more hero content

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,8 @@
 import Button from "../components/Button"
 import { arrowRight } from "../assets/icons"
+import { shoes, statistics } from "../constants"
+import { bigShoe1 } from "../assets/images"
+import ShoeCard from "../components/ShoeCard"
 const Hero = () => {
   return (
     <section 
@@ -7,16 +10,44 @@ const Hero = () => {
     className="flex flex-col justify-center w-full min-h-screen gap-10 xl:flex-row max-container"
     >
       <div className="relative flex flex-col items-start justify-center w-full xl:w-2/5 max-xl:padding-x pt-28">
-        <p className="">Our Summer Collections</p>
-        <h1>
-          <span>The New Arrivals</span>
+        <p className="text=xl font-montserrat text-coral-red">Our Summer Collections</p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="relative z-10 pr-10 xl:bg-white xl:whitespace-nowrap">The New Arrivals</span>
           <br />
-          <span>Nike Shoes</span>
+          <span className="inline-block mt-3 text-coral-red">Nike</span>
+          Shoes
         </h1>
-        <p>Discover stylish Nike arrivals, quality, comfort, and innovation 
+        <p className="mt-6 text-lg leading-8 font-montserrat sm:max-w-sm text-slate-gray mb-14">Discover stylish Nike arrivals, quality, comfort, and innovation 
           for your active life.
         </p>
         <Button label="Show Now" iconURL={arrowRight}/>
+        <div className="flex flex-wrap items-start justify-start w-full gap-16 mt-20">
+          {statistics.map((stat) => (
+            <div key={stat.label}>
+              <p className="text-4xl font-bold font-palanquin">{stat.value}</p>
+              <p className="font-mono leading-7 text-slate-gray">{stat.label}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="relative flex items-center justify-center flex-1 bg-center bg-cover xl:min-h-screen max-xl:py-40 bg-primary bg-hero">
+        <img src={bigShoe1} 
+              alt="Shoe Collection"
+              width={610}
+              height={500}
+              className="relative z-10 object-contain"
+        />      
+      </div>
+      <div>
+        {shoes.map((shoe) => {
+          <div key={shoe}>
+            <ShoeCard
+              imgURL={shoe}
+              changeBigShoeImage={() => {}}
+              bigShoeImg=""
+            />
+          </div>
+        })}
       </div>
     </section>
   )
